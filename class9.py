be98cab73d74b1c927569906841ee980412f46d6
coding8347587356 <utkarsh.casual@gmail.com>
2024-09-14 12:57:52 -0500

    Add files via upload

---
+++ class9.py
@@ -0,0 +1,23 @@
+#create a set
+set1 = {"apple", "banana", "cherry"}
+print(set1)
+
+#unordered
+#unindexed
+#unchangable
+#duplicates are not allowed
+
+set2 = {"apple", "banana", "cherry", "apple"}
+print(set2)
+
+set3 = {"apple", "banana", "cherry", True, 1}
+print(set3)
+
+#Get the lenght of a set
+print(len(set3))
+
+set4 = {"abc", 34, True, 40, "female"}
+print(set4)
+
+set5 = {"xyz", True, 98}
+print(type(set5))
